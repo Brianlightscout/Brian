7384606c8d64d99f5b17e19e7d22e44a0f35bb51
Brian Lucas <97444990+brianlucas07@users.noreply.github.com>
2023-04-01 15:40:16 -0300

    up

--- sites/portifólio/scripts/script.js
+++ sites/portifólio/scripts/script.js
@@ -1,8 +1,8 @@
 function clickMenu() {
-    if (menu_menor.style.display == 'none') {
-        menu_menor.style.display = 'block'
+    if (menu_menor.style.display == 'block') {
+        menu_menor.style.display = 'none'
         
     } else {
-        menu_menor.style.display = 'none'
+        menu_menor.style.display = 'block'
     }
 }
